9cf974801041b94fa77dfb72f9c5c785fadf9eec
THIAGO DANTAS <38766432+dantasthiago@users.noreply.github.com>
2022-07-20 13:42:48 -0300

    CPF brasilidades

---
+++ main.py
@@ -0,0 +1,26 @@
+from Cpf import Cpf
+
+cpf = '00068494238';
+# cpf = str(cpf);
+# tamahho_cpf = len(cpf);
+# print(tamahho_cpf);
+objeto_cpf = Cpf(cpf)
+
+# fatia_um = cpf[0:3]
+# fatia_dois = cpf[3:6]
+# fatia_tres = cpf[6:9]
+# fatia_quatro = cpf[9:]
+# cpf_formatado = '{}.{}.{}-{}'.format(
+#   fatia_um,
+#   fatia_dois,
+#   fatia_tres,
+#   fatia_quatro
+# )
+
+# print(fatia_um)
+# print(fatia_dois)
+# print(fatia_tres)
+# print(fatia_quatro)
+# print(cpf_formatado)
+
+print(objeto_cpf)
